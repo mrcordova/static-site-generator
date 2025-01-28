57b019a7a72abbfa3601cebf828c48c0b86a71d1
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-01-28 08:21:07 -0800

    leaf node creation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,16 +7,30 @@ class HTMLNode():
         self.children = children
         self.props = props
     
-    def to_htlm(self):
+    def to_html(self):
         raise NotImplementedError()
     
     def props_to_html(self):
+        if self.props is None:
+            return ""
         str = ""
         for key in self.props:
-            str += f' {key}="{self.props[key]}"'
-            
+            str += f' {key}="{self.props[key]}"'    
         return str
     
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
-    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError('Invalid HTML: no value')
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
